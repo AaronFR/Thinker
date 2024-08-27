59d766480fa93512f93ab0d9fe5f590ba27ce979
AaronFR <ferrisrichardsonaaron@gmail.com>
2024-08-27 11:25:33 +0100

    refactor: files are selected more judiciously

--- Personas/PersonaSpecification/SummariserSpecification.py
+++ Personas/PersonaSpecification/SummariserSpecification.py
@@ -1,2 +1,6 @@
+#ToDo: Documents which are themeselves are a powerful way of instructing the AI to follow tasks,
+# but for now we will avoid this until we can trigger it more deliberately
 SUMMARISER_SYSTEM_INSTRUCTIONS = """Take the file input and summarise it in a few lines.
-Note what the file is, what its category is and any notable flaws, features about the file"""
+Note what the file is, what its category is, notable features.
+
+Finally add a index of the files various parts chapters/functions etc, structurally summarising the document"""
--- Personas/PersonaSpecification/ThinkerSpecification.py
+++ Personas/PersonaSpecification/ThinkerSpecification.py
@@ -1,10 +1,13 @@
-from typing import List
+from typing import List, Tuple
 
 from Personas.PersonaSpecification.PersonaConstants import TYPE
 
 SELECT_FILES_FUNCTION_SCHEMA = [{
     "name": "executiveDirective",
-    "description": f"""Select from all above files given their summaries, which are appropriate for the given input""",
+    "description": f"""Select from all above files given their summaries, which are appropriate for the given input.
+    Be VERY selective, only include files if they explicitly relate to the given prompt. If a file has no relation or
+    is only tangentially related to a given prompt DO NOT INCLUDE IT.
+    By default your instinct should be to NOT INCLUDE A FILE""",
     "parameters": {
         TYPE: "object",
         "properties": {
