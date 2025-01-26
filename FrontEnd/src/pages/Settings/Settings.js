35e1aa99637d610f5a7c0c630875c5243c51906a
AaronFR <ferrisrichardsonaaron@gmail.com>
2025-01-26 21:17:01 +0000

    refactor[Settings.js]: Compartmentalise headings into their own components (And covertly fix a bug I introduced that prevented MessageSettings's wrapper Expanding textarea from inputting more than one character at a time 😅) (This entire change with very little modification is the result of a o1-mini request with a best of 3 value, I'm honestly shocked it managed to get it all right first time)

--- FrontEnd/src/pages/Settings/Settings.js
+++ FrontEnd/src/pages/Settings/Settings.js
@@ -3,9 +3,7 @@ import React, { useContext } from 'react';
 import { handleLogout } from '../../utils/loginUtils';
 import AutoExpandingTextarea from '../../utils/AutoExpandingTextarea';
 import TextSizeSlider from '../../utils/textSizeSlider';
-import { toTitleCase } from '../../utils/textUtils'
 import Navigation from '../../components/Navigation';
-
 
 import './Settings.css';
 
@@ -17,200 +15,383 @@ const FUNCTIONALITY_STATES = {
   AUTO: 'auto',
 };
 
+/**
+ * UserInterfaceSettings Component
+ *
+ * Renders User Interface related settings.
+ */
+const UserInterfaceSettings = ({ settings, toggleDarkMode, toggleAiColourisation }) => (
+  <div>
+    <h2 className="settings-heading">User Interface</h2>
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        checked={settings.darkMode}
+        onChange={toggleDarkMode}
+        className="settings-checkbox"
+      />
+      Dark Mode
+    </label>
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        checked={settings.aiColour}
+        onChange={toggleAiColourisation}
+        className="settings-checkbox"
+      />
+      New Category Colourisation via LLM Prompt
+    </label>
+
+    <TextSizeSlider />
+  </div>
+);
+
+/**
+ * FunctionalitySettings Component
+ *
+ * Renders Functionality related settings.
+ */
+const FunctionalitySettings = ({
+  settings,
+  changeSetting,
+  handleMessageChange,
+}) => (
+  <div>
+    <h2 className="settings-heading">Functionality</h2>
+
+    {/* Prompt Questioning Subsection */}
+    <PromptQuestioningSection
+      currentValue={settings.questionUserPromptsEnabled}
+      onChange={changeSetting}
+      promptMessage={settings.promptQuestioningMessage}
+      handleMessageChange={handleMessageChange}
+    />
+
+    {/* Auto Prompt Engineering Subsection */}
+    <AutoPromptEngineeringSection
+      currentValue={settings.augmentedPromptsEnabled}
+      onChange={changeSetting}
+      promptMessage={settings.promptAugmentationMessage}
+      handleMessageChange={handleMessageChange}
+    />
+  </div>
+);
+
+/**
+ * PromptQuestioningSection Component
+ *
+ * Renders settings for Prompt Questioning.
+ */
+const PromptQuestioningSection = ({
+  currentValue,
+  onChange,
+  promptMessage,
+  handleMessageChange,
+}) => (
+  <div>
+    <h3>Prompt Questioning</h3>
+    <label className="settings-label">
+      <select
+        className="settings-select"
+        value={currentValue}
+        onChange={(e) =>
+          onChange(
+            'beta_features.question_user_prompts_enabled',
+            e.target.value,
+            'questionUserPromptsEnabled'
+          )
+        }
+      >
+        <option value={FUNCTIONALITY_STATES.OFF}>Off</option>
+        <option value={FUNCTIONALITY_STATES.ON}>On</option>
+        <option value={FUNCTIONALITY_STATES.AUTO}>Auto</option>
+      </select>
+      Generates questions against the user's prompt and reference material.
+    </label>
+    <p>Use case: Difficult, 'knotty' technical questions.</p>
+    <AutoExpandingTextarea
+      value={promptMessage}
+      className="textarea"
+      onChange={(e) =>
+        handleMessageChange('promptQuestioningMessage', e.target.value)
+      }
+      style={{ opacity: 0.9 }}
+    />
+  </div>
+);
+
+/**
+ * AutoPromptEngineeringSection Component
+ *
+ * Renders settings for Auto Prompt Engineering.
+ */
+const AutoPromptEngineeringSection = ({
+  currentValue,
+  onChange,
+  promptMessage,
+  handleMessageChange,
+}) => (
+  <div>
+    <h3>Auto Prompt Engineering</h3>
+    <label className="settings-label">
+      <select
+        className="settings-select"
+        value={currentValue}
+        onChange={(e) =>
+          onChange(
+            'beta_features.augmented_prompts_enabled',
+            e.target.value,
+            'augmentedPromptsEnabled'
+          )
+        }
+      >
+        <option value={FUNCTIONALITY_STATES.OFF}>Off</option>
+        <option value={FUNCTIONALITY_STATES.ON}>On</option>
+        <option value={FUNCTIONALITY_STATES.AUTO}>Auto</option>
+      </select>
+      Generates a copy of your prompt that meets 'prompt engineering' standards.
+    </label>
+    <p>Use case: simple prompts that can-benefit from refinement.</p>
+    <AutoExpandingTextarea
+      value={promptMessage}
+      className="textarea"
+      onChange={(e) =>
+        handleMessageChange('promptAugmentationMessage', e.target.value)
+      }
+      style={{ opacity: 0.9 }}
+    />
+  </div>
+);
+
+/**
+ * SummariesSettings Component
+ *
+ * Renders Summaries related settings.
+ */
+const SummariesSettings = ({
+  settings,
+  toggleSummarisation,
+  handleMessageChange,
+}) => (
+  <div>
+    <h2 className="settings-heading">Summaries</h2>
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        className="settings-checkbox"
+        id="summarise-checkbox"
+        checked={settings.summarisationEnabled}
+        onChange={toggleSummarisation}
+      />
+      Enables summaries on compatible workflows
+    </label>
+    <AutoExpandingTextarea
+      value={settings.summarisationMessage}
+      className="textarea"
+      onChange={(e) =>
+        handleMessageChange('summarisationMessage', e.target.value)
+      }
+      style={{ opacity: 0.9 }}
+    />
+  </div>
+);
+
+/**
+ * BetaFeaturesSettings Component
+ *
+ * Renders Beta Features related settings.
+ */
+const BetaFeaturesSettings = ({
+  settings,
+  toggleDebug,
+  toggleUserEncyclopedia,
+  toggleEncyclopedia,
+  toggleMultiFileProcessing,
+}) => (
+  <div>
+    <h2 className="settings-heading">🚧 Beta Features</h2>
+    <label className="settings-label" style={{ paddingBottom: '30px' }}>
+      <input
+        type="checkbox"
+        className="settings-checkbox"
+        id="debug-checkbox"
+        checked={settings.debug}
+        onChange={toggleDebug}
+      />
+      Enable debug view (Directly view prompt tags)
+    </label>
+
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        checked={settings.userEncyclopediaEnabled}
+        onChange={toggleUserEncyclopedia}
+        className="settings-checkbox"
+      />
+      User knowledge - The thinker will remember details about the user and their preferences (user
+      preferences and facts are accumulated when enabled but not currently used when prompting)
+    </label>
+
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        checked={settings.encyclopediaEnabled}
+        onChange={toggleEncyclopedia}
+        className="settings-checkbox"
+      />
+      Reference knowledge - The thinker will look up details online (Wikipedia currently) and use
+      them in reference to your prompt where appropriate
+    </label>
+
+    <label className="settings-label">
+      <input
+        type="checkbox"
+        checked={settings.multiFileProcessingEnabled}
+        onChange={toggleMultiFileProcessing}
+        className="settings-checkbox"
+      />
+      Multi file processing - personas can operate on multiple files at once (unstable)
+    </label>
+  </div>
+);
+
+/**
+ * SystemMessagesSettings Component
+ *
+ * Renders System Messages related settings without using the MessageSettings component.
+ */
+const SystemMessagesSettings = ({ settings, handleMessageChange }) => (
+  <div>
+    <h2 className="settings-heading">System Messages</h2>
+    <div className="message-settings">
+      {/* Coder Persona Message */}
+      <label className="message-label">
+        Coder Persona Message
+        <AutoExpandingTextarea
+          value={settings.coderPersonaMessage}
+          className="textarea"
+          onChange={(e) =>
+            handleMessageChange('coderPersonaMessage', e.target.value)
+          }
+          style={{ opacity: 0.9 }}
+        />
+      </label>
+
+      {/* Writer Persona Message */}
+      <label className="message-label">
+        Writer Persona Message
+        <AutoExpandingTextarea
+          value={settings.writerPersonaMessage}
+          className="textarea"
+          onChange={(e) =>
+            handleMessageChange('writerPersonaMessage', e.target.value)
+          }
+          style={{ opacity: 0.9 }}
+        />
+      </label>
+
+      {/* Categorisation Message */}
+      <label className="message-label">
+        Categorisation Message
+        <AutoExpandingTextarea
+          value={settings.categorisationMessage}
+          className="textarea"
+          onChange={(e) =>
+            handleMessageChange('categorisationMessage', e.target.value)
+          }
+          style={{ opacity: 0.9 }}
+        />
+      </label>
+
+      {/* Best Of Message */}
+      <label className="message-label">
+        Best Of Message
+        <AutoExpandingTextarea
+          value={settings.bestOfMessage}
+          className="textarea"
+          onChange={(e) =>
+            handleMessageChange('bestOfMessage', e.target.value)
+          }
+          style={{ opacity: 0.9 }}
+        />
+      </label>
+    </div>
+  </div>
+);
 
 /**
  * Settings Component
  *
- * Renders the settings interface, allowing users to modify various application preferences.
+ * Main component that aggregates all settings sections.
  */
 export function Settings() {
-  const { 
-      settings,
-      changeSetting,
-      toggleSetting,
-      handleMessageChange
+  const {
+    settings,
+    changeSetting,
+    toggleSetting,
+    handleMessageChange,
   } = useContext(SettingsContext);
 
+  // Toggle Handlers
   const toggleDebug = () => toggleSetting('interface.debug', 'debug');
-  const toggleDarkMode = () => toggleSetting('interface.dark_mode', 'darkMode');
-  const toggleAiColourisation = () => toggleSetting('interface.ai_colour', 'aiColour')
-  const toggleUserEncyclopedia = () => toggleSetting('beta_features.user_context_enabled', 'userEncyclopediaEnabled')
-  const toggleEncyclopedia = () => toggleSetting('beta_features.encyclopedia_enabled', 'encyclopediaEnabled') 
-  const toggleMultiFileProcessing = () => toggleSetting('beta_features.multi_file_processing_enabled', 'multiFileProcessingEnabled')
-  const togglesummarisation = () => toggleSetting('optimization.summarise', 'summarisationEnabled')
-
-  const uiOptions = [
-      { label: "Dark Mode", value: settings.darkMode, onChange: toggleDarkMode },
-      { label: "New Category Colourisation via LLM Prompt", value: settings.aiColour, onChange: toggleAiColourisation }
-  ];
-
-  const betaOptions = [
-      { 
-          label: "User knowledge - The thinker will remember details about the user and their preferences (user preferences and facts are accumulated when enabled but not currently used when prompting)",
-          value: settings.userEncyclopediaEnabled, 
-          onChange: toggleUserEncyclopedia
-      },
-      { 
-          label: "Reference knowledge - The thinker will look up details online (Wikipedia currently) and use them in reference to your prompt where appropriate",
-          value: settings.encyclopediaEnabled, 
-          onChange: toggleEncyclopedia 
-      },
-      { 
-          label: "Multi file processing - personas can operate on multiple files at once (unstable)",
-          value: settings.multiFileProcessingEnabled, 
-          onChange: toggleMultiFileProcessing
-      },
-  ];
-
-  const PromptQuestioningSection = ({ currentValue, onChange, promptMessage, handleMessageChange }) => (
-    <>
-      <h2>Prompt Questioning</h2>
-      <label className="settings-label">
-        <select
-          className="settings-select"
-          value={currentValue}
-          onChange={(e) => onChange('beta_features.question_user_prompts_enabled', e.target.value, 'questionUserPromptsEnabled')}
-        >
-          <option value={FUNCTIONALITY_STATES.OFF}>Off</option>
-          <option value={FUNCTIONALITY_STATES.ON}>On</option>
-          <option value={FUNCTIONALITY_STATES.AUTO}>Auto</option>
-        </select>
-        Generates questions against the user's prompt and reference material.
-      </label>
-      <p>Use case: Difficult, 'knotty' technical questions.</p>
-      <AutoExpandingTextarea
-        value={promptMessage}
-        className='textarea'
-        onChange={(e) => handleMessageChange('promptQuestioningMessage', e.target.value)}
-        style={{ opacity: 0.9 }}
-      />
-    </>
-  );
-
-  const AutoPromptEngineeringSection = ({ currentValue, onChange, promptMessage, handleMessageChange }) => (
-    <>
-      <h2>Auto Prompt Engineering</h2>
-      <label className="settings-label">
-        <select
-          className="settings-select"
-          value={currentValue}
-          onChange={(e) => onChange('beta_features.augmented_prompts_enabled', e.target.value, 'augmentedPromptsEnabled')}
-        >
-          <option value={FUNCTIONALITY_STATES.OFF}>Off</option>
-          <option value={FUNCTIONALITY_STATES.ON}>On</option>
-          <option value={FUNCTIONALITY_STATES.AUTO}>Auto</option>
-        </select>
-        Generates a copy of your prompt that meets 'prompt engineering' standards.
-      </label>
-      <p>Use case: simple prompts that can-benefit from refinement.</p>
-      <AutoExpandingTextarea
-        value={promptMessage}
-        className='textarea'
-        onChange={(e) => handleMessageChange('promptAugmentationMessage', e.target.value)}
-        style={{ opacity: 0.9 }}
-      />
-    </>
-  );
-
-  /**
-   * Renders a System Message setting with a text area.
-   */
-  const MessageSettings = ({ label, value, onChange }) => (
-    <label className="message-label">
-      {label}
-      <AutoExpandingTextarea
-        value={value}
-        className='textarea'
-        onChange={onChange}
-        style={{ opacity: 0.9 }}
-      />
-    </label>
-  );
+  const toggleDarkMode = () =>
+    toggleSetting('interface.dark_mode', 'darkMode');
+  const toggleAiColourisation = () =>
+    toggleSetting('interface.ai_colour', 'aiColour');
+  const toggleUserEncyclopedia = () =>
+    toggleSetting(
+      'beta_features.user_context_enabled',
+      'userEncyclopediaEnabled'
+    );
+  const toggleEncyclopedia = () =>
+    toggleSetting('beta_features.encyclopedia_enabled', 'encyclopediaEnabled');
+  const toggleMultiFileProcessing = () =>
+    toggleSetting(
+      'beta_features.multi_file_processing_enabled',
+      'multiFileProcessingEnabled'
+    );
+  const toggleSummarisation = () =>
+    toggleSetting('optimization.summarise', 'summarisationEnabled');
 
   return (
     <div className="scrollable settings-container">
       <Navigation />
 
-      {/* User Interface Section */}
-      <h2 className="settings-heading">User Interface</h2>
-      {uiOptions.map(({ label, value, onChange }, index) => (
-        <label key={index} className="settings-label">
-          <input type="checkbox" checked={value} onChange={onChange} className="settings-checkbox" />
-          {label}
-        </label>
-      ))}
-
-      <TextSizeSlider />
-
-      {/* Functionality Section */}
-      <h2 className="settings-heading">Functionality</h2>
-      
-      <PromptQuestioningSection 
-          currentValue={settings.questionUserPromptsEnabled}
-          onChange={changeSetting}
-          promptMessage={settings.promptQuestioningMessage}
-          handleMessageChange={handleMessageChange}
+      <UserInterfaceSettings
+        settings={settings}
+        toggleDarkMode={toggleDarkMode}
+        toggleAiColourisation={toggleAiColourisation}
       />
 
-      <AutoPromptEngineeringSection 
-          currentValue={settings.augmentedPromptsEnabled}
-          onChange={changeSetting}
-          promptMessage={settings.promptAugmentationMessage}
-          handleMessageChange={handleMessageChange}
+      <FunctionalitySettings
+        settings={settings}
+        changeSetting={changeSetting}
+        handleMessageChange={handleMessageChange}
       />
 
-      <h2>Summaries</h2>
-      <label className="settings-label">
-        <input
-          type="checkbox"
-          className="settings-checkbox"
-          id="summarise-checkbox"
-          checked={settings.summarisationEnabled}
-          onChange={togglesummarisation}
-        />
-        Enables summaries on compatible workflows
-      </label>
-      <AutoExpandingTextarea
-        value={settings.summarisationMessage}
-        className='textarea'
-        onChange={(e) => handleMessageChange('summarisationMessage', e.target.value)}
-        style={{ opacity: 0.9 }}
+      <SummariesSettings
+        settings={settings}
+        toggleSummarisation={toggleSummarisation}
+        handleMessageChange={handleMessageChange}
       />
 
-      {/* Beta Features Section */}
-      <h2 className="settings-heading">🚧 Beta features</h2>
-      <label className="settings-label" style={{"paddingBottom": "30px"}}>
-        <input
-          type="checkbox"
-          className="settings-checkbox"
-          id="debug-checkbox"
-          checked={settings.debug}
-          onChange={toggleDebug}
-        />
-        Enable debug view (Directly view prompt tags)
-      </label>
-      {betaOptions.map(({ label, value, onChange }, index) => (
-        <label key={index} className="settings-label">
-          <input type="checkbox" checked={value} onChange={onChange} className="settings-checkbox" />
-          {label}
-        </label>
-      ))}
+      <SystemMessagesSettings
+        settings={settings}
+        handleMessageChange={handleMessageChange}
+      />
 
-      {/* System Messages Section */}
-      <h2 className="settings-heading">System Messages</h2>
-      <div className="message-settings">
-        {['coderPersonaMessage', 'writerPersonaMessage', 'categorisationMessage', 'bestOfMessage'].map((msgType, index) => (
-          <MessageSettings
-            key={index}
-            label={toTitleCase(msgType.replace(/([A-Z])/g, ' $1'))}
-            value={settings[msgType]}
-            onChange={(e) => handleMessageChange(msgType, e.target.value)}
-          />
-        ))}
-      </div>
+      <BetaFeaturesSettings
+        settings={settings}
+        toggleDebug={toggleDebug}
+        toggleUserEncyclopedia={toggleUserEncyclopedia}
+        toggleEncyclopedia={toggleEncyclopedia}
+        toggleMultiFileProcessing={toggleMultiFileProcessing}
+      />
 
-      <div className='center-contents'>
-        <button onClick={handleLogout} className="centered logout-button">Logout</button>
+      <div className="center-contents">
+        <button onClick={handleLogout} className="centered logout-button">
+          Logout
+        </button>
       </div>
     </div>
   );
